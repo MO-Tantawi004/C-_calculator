5f4974bbf70b3fa2a612c1851a5a79f8ff05bf2e
Mohamed Naser <mohamedtantaway9@gmail.com>
2025-07-30 16:23:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿namespace MNF_S2_Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Console
+            /*
+            Console.Title = "C# Day1";
+            Console.WriteLine("Hello");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Danger");
+            Console.ResetColor();
+            Console.Write("Danger");
+            */
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Naming & Declare
+
+            //int x = 10;
+
+            //int @int = 5;
+
+            //int _2 = 5;
+
+            //int _5 = 5;
+
+
+            #endregion
+
+            #region Datatypes
+            //int, double, long, short, decaiml, float, bool, char, string
+
+            //Console.WriteLine(sizeof(int)); //4 
+            //Console.WriteLine(sizeof(long));
+            //Console.WriteLine(sizeof(double));
+            //Console.WriteLine(sizeof(decimal));
+            //Console.WriteLine(sizeof(float));
+            //Console.WriteLine(sizeof(bool));
+            //Console.WriteLine(sizeof(char));
+
+
+            //Console.WriteLine(byte.MinValue);
+            //Console.WriteLine(byte.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(uint.MinValue);
+            //Console.WriteLine(uint.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(long.MinValue);
+            //Console.WriteLine(long.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(ulong.MinValue);
+            //Console.WriteLine(ulong.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(decimal.MinValue);
+            //Console.WriteLine(decimal.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(double.MinValue);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine(float.MinValue);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine("===========================");
+
+
+            #endregion
+
+            #region String
+
+            //string firstName = "Amr";
+            //string lastName = "Khaled";
+
+            ////string fullName = firstName + ' ' + lastName;
+            ////Console.WriteLine(fullName);
+
+            //string FullName = $"{firstName} {lastName}";
+            //Console.WriteLine(FullName);
+
+            //Console.WriteLine(FullName.ToUpper());
+            //Console.WriteLine(FullName.ToLower());
+            //Console.WriteLine(FullName.Length);
+            //Console.WriteLine(FullName.IndexOf('R'));//0 -1
+            //Console.WriteLine(FullName.Contains(value: 'R',StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine(FullName.Replace("amr","omar",StringComparison.OrdinalIgnoreCase));
+
+            #endregion
+
+            #region User Input
+
+            // ReadLine() => string
+
+            //Console.Write("Enter Your Name: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("Enter Your Age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Name: {name}, age: {age}");
+
+
+            //Read()
+
+            //Console.Write("Enter Key: ");
+            //char k2 = Convert.ToChar(Console.Read());
+            //Console.ReadLine();
+            //char k3 = (char)Console.Read();
+            //Console.ReadLine();
+
+            //int k1 = Console.Read();
+            //Console.ReadLine();
+
+            //Console.WriteLine(k1); //97
+            //Console.WriteLine(k2); // a
+            //Console.WriteLine(k3); // a
+            #endregion;
+        }
+    }
+}
